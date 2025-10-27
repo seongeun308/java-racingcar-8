2eeea9de5f2d3bf4ad4277a09548726882293d12
seongeun308 <kse011010@gmail.com>
2025-10-27 23:04:04 +0900

    refactor: 애플리케이션 진입점 CarApp 생성 및 리팩토링
    - CarApp으로 구조 개선 및 실행 흐름 분리

---
+++ src/main/java/racingcar/app/CarApp.java
@@ -0,0 +1,60 @@
+package racingcar.app;
+
+import racingcar.domain.Car;
+import racingcar.parser.InputParser;
+import racingcar.util.CarUtils;
+import racingcar.validator.InputValidator;
+import racingcar.view.InputView;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CarApp {
+
+    private final InputView inputView;
+    private final InputValidator inputValidator;
+    private final InputParser inputParser;
+
+    public CarApp(InputView inputView, InputValidator inputValidator, InputParser inputParser) {
+        this.inputView = inputView;
+        this.inputValidator = inputValidator;
+        this.inputParser = inputParser;
+    }
+
+    public void run() {
+        String namesInput = inputView.readLine("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String roundInput = inputView.readLine("시도할 횟수는 몇 회인가요?");
+
+        inputValidator.validateNotBlank(namesInput);
+        String[] names = inputParser.parseNames(namesInput);
+
+        inputValidator.validateRange(names.length, 1, 5);
+        validateLengthNames(names);
+        inputValidator.validateUniqueNames(names);
+
+        inputValidator.validateNotBlank(roundInput);
+        int round = inputParser.parseRound(roundInput);
+
+        inputValidator.validateRange(round, 1, 10);
+
+        List<Car> cars = generateCars(names);
+
+        for (int i = 1; i <= round; i++) {
+            cars.forEach(car -> {
+                int step = CarUtils.generateStep();
+                car.move(step);
+            });
+        }
+    }
+
+    private static List<Car> generateCars(String[] names) {
+        return Arrays.stream(names)
+                .map(Car::new)
+                .toList();
+    }
+
+    private void validateLengthNames(String[] names) {
+        Arrays.stream(names)
+                .forEach(input -> inputValidator.validateLength(input, 1, 10));
+    }
+}
